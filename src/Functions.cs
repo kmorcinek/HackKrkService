4e123a9f990e372c6e2166e3abfe2eceebc430e2
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2013-07-25 20:51:11 +0200

    functions

---
+++ src/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NancyTest
+{
+    public class Functions
+    {
+        public Dictionary<string, int> NameToId = new Dictionary<string, int>();
+        public Dictionary<int, Func<int, int, int>> IdToFunction = new Dictionary<int, Func<int, int, int>>();
+
+        public Functions()
+        {
+            NameToId["add"] = 3;
+            IdToFunction[3] = (x, y) => x + y;
+        }
+    }
+}
---
+++ src/FunctionsModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace NancyTest
+{
+    public class FunctionsModule : NancyModule
+    {
+        public FunctionsModule()
+        {
+            Get["/functions/builtin/{name}"] = args =>
+                {
+                    return Response.AsJson(new {id = 3});
+                };
+        }
+    }
+}
